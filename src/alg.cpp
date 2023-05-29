54620a95f95ae66366f0b99c220457c94bb49478
StepanNaydin <124874525+StepanNaydin@users.noreply.github.com>
2023-05-29 23:09:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,11 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    std::vector<char> result;
+    for (int q = 0; q < tree.all.size(); q++) {
+        if (q >= (n - 1) * tree.len && q < n * tree.len) {
+            result.push_back(tree.all[q]);
+        }
+    }
+    return result;
 }
